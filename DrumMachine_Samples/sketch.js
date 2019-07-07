27747259f8db81d26eb561776aefa671717f7fef
PaulChen <bernie40916@gmail.com>
2019-07-07 18:48:58 +0800

    add touch

--- DrumMachine_Samples/sketch.js
+++ DrumMachine_Samples/sketch.js
@@ -88,7 +88,18 @@ function mousePressed() {
     }
 }
   
-
+function touchStarted() {
+    if (hh.isLoaded() && clap.isLoaded() && bass.isLoaded() && sound.isLoaded()) {
+      if (!drums.isPlaying) {
+        drums.metro.metroTicks = 0;
+        drums.loop();
+      } else {
+        drums.stop();
+      }
+    } else {
+      console.log('oops, be patient as drums load...');
+    }
+}
 function keyPressed(){
     if (hh.isLoaded() && clap.isLoaded() && bass.isLoaded() && sound.isLoaded()) {
       if(!drums.isPlaying) {
